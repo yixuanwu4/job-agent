e16fee94b14165afc570a5228ceed05dcab4920f
Yixuan <y+github@ixuan.ch>
2026-07-29 13:00:39 +0000

    chore: format document

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -1,7 +1,6 @@
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
-from io import BufferedReader
 
 load_dotenv()
 
@@ -9,25 +8,44 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_SECRET_KEY")
 supabase: Client = create_client(url, key)
 
-def add_subscriber(email: str, role: str, location: str, country: str, preferred_language: str, cv_storage_path: str):
+
+def add_subscriber(
+    email: str,
+    role: str,
+    location: str,
+    country: str,
+    preferred_language: str,
+    cv_storage_path: str,
+):
     response = (
-        supabase.table("subscribers").insert({"email": email, "role": role, "location": location, "country": country, "preferred_language": preferred_language, "cv_storage_path": cv_storage_path, "active": True}).execute()
+        supabase.table("subscribers")
+        .insert(
+            {
+                "email": email,
+                "role": role,
+                "location": location,
+                "country": country,
+                "preferred_language": preferred_language,
+                "cv_storage_path": cv_storage_path,
+                "active": True,
+            }
+        )
+        .execute()
     )
     return response
 
+
 def get_active_subscribers():
-    response = (
-        supabase.table("subscribers").select("*").eq("active", True).execute()
-    )
+    response = supabase.table("subscribers").select("*").eq("active", True).execute()
     return response.data
+
 
 def upload_cv(file_bytes: bytes, filename: str, email: str) -> str:
     path = f"{email}/{filename}"
     supabase.storage.from_("resumes").upload(
-        path = path,
-        file = file_bytes,
-        file_options = {"upsert": "true"}
+        path=path, file=file_bytes, file_options={"upsert": "true"}
     )
+
 
 with open("cv.pdf", "rb") as f:
     file_bytes = f.read()
