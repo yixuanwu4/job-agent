e4095d338d232989baab3f641885856467cf6633
Yixuan <y+github@ixuan.ch>
2026-07-29 13:02:26 +0000

    chore: format document

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -1,6 +1,7 @@
-from supabase import create_client, Client
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from supabase import Client, create_client
 
 load_dotenv()
 
