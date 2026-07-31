7ad7589c5e95daffe53ec4af4358a66eca331ec8
Yixuan <y+github@ixuan.ch>
2026-07-31 19:26:37 +0000

    fix: add localhost address to CORS whitelist

--- backend/api.py
+++ backend/api.py
@@ -14,7 +14,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://yixuanwu4.github.io"],
+    allow_origins=[
+        "https://yixuanwu4.github.io",
+        "http://localhost:5173",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
