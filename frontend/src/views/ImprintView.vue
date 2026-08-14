e676945b745c660287889feba43ac26b0cc7d4bb
Yixuan <y+github@ixuan.ch>
2026-08-14 11:14:13 +0000

    docs: update imprint text

--- frontend/src/views/ImprintView.vue
+++ frontend/src/views/ImprintView.vue
@@ -2,20 +2,26 @@
   <div class="legal-page">
     <h1>Imprint</h1>
     <p>The identity of the owner of this application is stated below.</p>
-    <div>
+    <div id="general">
       <p><strong>Yixuan Wu</strong></p>
       <p>c/o openscript GmbH</p>
       <p>Europa-Strasse 30</p>
       <p>8152 Glattbrugg</p>
       <p><a href="mailto:job-agent@yixuan.ch">job-agent@yixuan.ch</a></p>
-
-      <p>
-        This is a personal, non-commercial project, provided free of charge. It is not operated by a
-        registered company.
-      </p>
-
-      <p>Responsible for the content of this website: Yixuan Wu, contact details as above.</p>
     </div>
+
+    <div id="profile">
+        <h2>The profile</h2>
+        <p>This project is presented on the following platform:</p>
+        <ul>
+            <li>
+                <a href="https://github.com/yixuanwu4/job-agent">
+                    GitHub
+                </a>
+            </li>
+        </ul>
+    </div>
+    
   </div>
 </template>
 
@@ -25,8 +31,25 @@
   margin: 0 auto;
   padding: var(--space-8) var(--space-5);
 }
-.legal-page p {
-  margin-bottom: var(--space-3);
+
+p {
+  line-height: 1.6;
   color: var(--color-text-muted);
+}
+
+a {
+    text-decoration: underline;
+}
+
+li {
+    line-height: 1.6;
+}
+
+#general {
+    margin-block-start: var(--space-4);
+}
+
+#profile {
+    margin-top: var(--space-8);
 }
 </style>
